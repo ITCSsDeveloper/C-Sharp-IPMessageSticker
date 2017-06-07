1092757926233992ed5b054926fb14a47dd7a4a2
ITCS'sDeveloper <itcss.developer.me@gmail.com>
2017-06-07 10:03:42 +0700

    Add cClipboard

---
+++ C-Sharp-IPMessageSticker/C-Sharp-IPMessageSticker/cClipboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace C_Sharp_IPMessageSticker
+{
+    public class cClipboard
+    {
+        public static void AddToClipboard(Image img)
+        {
+            
+        }
+
+
+    }
+}
