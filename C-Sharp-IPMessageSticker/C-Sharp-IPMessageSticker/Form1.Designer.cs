35e7b761114bca22b797bb7792abc2619cafd2ad
Krittin-Eddy <krittin@iconext.co.th>
2017-06-07 12:54:41 +0700

    Add UI

--- C-Sharp-IPMessageSticker/C-Sharp-IPMessageSticker/Form1.Designer.cs
+++ C-Sharp-IPMessageSticker/C-Sharp-IPMessageSticker/Form1.Designer.cs
@@ -29,11 +29,71 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listViewChild = new System.Windows.Forms.ListView();
+            this.listViewParent = new System.Windows.Forms.ListView();
+            this.imageListChild = new System.Windows.Forms.ImageList(this.components);
+            this.imageListParent = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // listViewChild
+            // 
+            this.listViewChild.BackColor = System.Drawing.SystemColors.Control;
+            this.listViewChild.Location = new System.Drawing.Point(3, 43);
+            this.listViewChild.Name = "listViewChild";
+            this.listViewChild.Size = new System.Drawing.Size(293, 217);
+            this.listViewChild.TabIndex = 0;
+            this.listViewChild.UseCompatibleStateImageBehavior = false;
+            // 
+            // listViewParent
+            // 
+            this.listViewParent.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listViewParent.BackColor = System.Drawing.Color.Gainsboro;
+            this.listViewParent.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listViewParent.LabelWrap = false;
+            this.listViewParent.Location = new System.Drawing.Point(1, 2);
+            this.listViewParent.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.listViewParent.MultiSelect = false;
+            this.listViewParent.Name = "listViewParent";
+            this.listViewParent.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listViewParent.Scrollable = false;
+            this.listViewParent.ShowGroups = false;
+            this.listViewParent.Size = new System.Drawing.Size(297, 40);
+            this.listViewParent.TabIndex = 0;
+            this.listViewParent.UseCompatibleStateImageBehavior = false;
+            this.listViewParent.SelectedIndexChanged += new System.EventHandler(this.listViewParent_SelectedIndexChanged);
+            // 
+            // imageListChild
+            // 
+            this.imageListChild.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageListChild.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageListChild.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageListParent
+            // 
+            this.imageListParent.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageListParent.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageListParent.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 262);
+            this.Controls.Add(this.listViewChild);
+            this.Controls.Add(this.listViewParent);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewChild;
+        private System.Windows.Forms.ListView listViewParent;
+        private System.Windows.Forms.ImageList imageListChild;
+        private System.Windows.Forms.ImageList imageListParent;
+
     }
 }
 
